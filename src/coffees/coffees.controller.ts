eee708b9f977e9d2a1aca2800c72002ec728879c
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-06-24 20:13:06 +0200

    findOne

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get('flavours')
+    @Get()
     findAll() {
         return "Get all coffees";
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action return #${id} coffee`;
     }
 }
