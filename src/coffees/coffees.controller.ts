c83326e3b26f523106478d7e3ecd9eeb8a85c40d
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-06-24 20:47:59 +0200

    user service provider

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,31 +1,37 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+    constructor(
+        private readonly coffeesService: CoffeesService,
+    ) {
+
+    }
     @Get()
     @HttpCode(200)
     findAll(@Query() paginationQuery) {
         const { limit, offset } = paginationQuery;
-        return `This action returns all coffees. Limit ${limit}, offset: ${offset}`;
+        return this.coffeesService.findAll();
     }
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return `This action return #${id} coffee`;
+        return this.coffeesService.findOne(id);
     }
 
     @Post()
     create(@Body() body) {
-        return body;
+        return this.coffeesService.create(body);
     }
 
     @Patch(':id')
     update(@Param('id') id: string, @Body() body) {
-        return `This action updates #${id} coffee`;
+        return this.coffeesService.update(id, body);
     }
 
     @Delete(':id')
     remove(@Param('id') id: string) {
-        return `This action removes #${id} coffee`;
+        return this.coffeesService.remove(id);
     }
 }
